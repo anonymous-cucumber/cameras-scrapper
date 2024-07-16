8eb789a000449a52407ceaa246f05255199eedb0
Anonymous Developper <anonymous.developper@proton.me>
2024-07-16 23:39:12 +0200

    Continuing developping 'surveillanceUnderSurveillance' scrapping, need to be finished and tested

---
+++ project/libs/promiseConcurrency.js
@@ -0,0 +1,17 @@
+async function promiseConcurrency(promises, concurrency, betweenCallback = null) {
+    lists = promises.reduce((acc,promise) => 
+        acc[acc.length-1]?.length < concurrency ?
+            acc.slice(0,-1).concat([(acc[acc.length-1]??[]).concat([promise])]) :
+            acc.concat([[promise]])
+    , [])
+
+    let res = [];
+    for (let i=0;i<lists.length;i++) {
+        const promises = lists[i];
+        res = res.concat(await Promise.all(promises));
+        if (betweenCallback !== null) betweenCallback(i+1,lists.length);
+    }
+    return res;
+}
+
+module.exports = promiseConcurrency;
--- project/scrappers/surveillanceUnderSurveillance.js
+++ project/scrappers/surveillanceUnderSurveillance.js
@@ -1,7 +1,6 @@
-const {calcDistanceBetween,destinationPointLat,destinationPointLon} = require("../libs/convert");
-
 const bboxes = {
-    france: {lat1: 42.332792, lon1: -5.142053, lat2: 51.089147, lon2: 8.233550}
+    france: {lat1: 42.332792, lon1: -5.142053, lat2: 51.089147, lon2: 8.233550},
+    paris: {lat1: 48.815729, lon1: 2.258395, lat2: 48.901457, lon2: 2.415771}
 };
 
 const searchsSize = {horizontal: 1, vertical: 1} // Exprimed in degree
@@ -12,33 +11,50 @@ function valid(bbox) {
     return {success: false, msg: `The bbox "${bbox}" does not exist`}
 }
 
-function scrapper({lat1, lon1, lat2, lon2}) {
-    const verticalDistance = calcDistanceBetween(lat1,lon1,lat2,lon1);
+function getHorizontalSearchBBoxes(searchLat1, searchLat2, searchLon1, horizontalNbSearchs, horizontalModulo) {
+    const bboxes = [];
+    for (let j=0;j<horizontalNbSearchs;j++) {
+        const searchLon2 = searchLon1+searchsSize.horizontal;
+
+        bboxes.push({searchLat1, searchLon1, searchLat2, searchLon2});
+
+        searchLon1 = searchLon2
+    }
+    const searchLon2 = searchLon1+horizontalModulo;
+    bboxes.push({searchLat1, searchLon1, searchLat2, searchLon2});
+    return bboxes;
+}
+function getSearchBBoxes({lat1, lon1, lat2, lon2}) {
+    const verticalDistance = lat2-lat1;
     const verticalNbSearchs = Math.floor(verticalDistance/searchsSize.vertical);
     const verticalModulo = verticalDistance%searchsSize.vertical;
+
+    const horizontalDistance = lon2-lon1;
+    const horizontalNbSearchs = Math.floor(horizontalDistance/searchsSize.horizontal);
+    const horizontalModulo = horizontalDistance%searchsSize.horizontal;
+
+    let bboxes = [];
 
     let searchLat1 = lat1;
     let searchLon1 = lon1;
     for (let i=0;i<verticalNbSearchs;i++) {
-        const searchLat2 = destinationPointLat(searchLat1, false, searchsSize.vertical);
+        const searchLat2 = searchLat1+searchsSize.vertical;
 
-        const horizontalDistance = calcDistanceBetween(searchLat2,lon1,searchLat2,lon2);
-        const horizontalNbSearchs = Math.floor(horizontalDistance/searchsSize.horizontal);
-        const horizontalModulo = horizontalDistance%searchsSize.horizontal;
-        console.log({searchLat1, searchLat2, horizontalDistance});
+        console.log("VERTICAL ----------------------------------------------------------------------------------------------------------------------")
 
-        for (let j=0;j<horizontalNbSearchs;j++) {
-            const searchLon2 = destinationPointLon(searchLat2, searchLon1, true, searchsSize.horizontal);
-
-            //console.log({lat1, lon1, lat2, lon2});
-
-            searchLon1 = searchLon2
-        }
-        const searchLon2 = destinationPointLon(searchLat2, lon1, true, horizontalModulo);
+        bboxes = bboxes.concat(getHorizontalSearchBBoxes(searchLat1, searchLat2, searchLon1, horizontalNbSearchs, horizontalModulo));
 
         searchLat1 = searchLat2;
+        searchLon1 = lon1;
     }
-    const searchLat2 = destinationPointLat(searchLat1, false, verticalModulo);
+    const searchLat2 = searchLat1+verticalModulo;
+    bboxes = bboxes.concat(getHorizontalSearchBBoxes(searchLat1, searchLat2, searchLon1, horizontalNbSearchs, horizontalModulo));
+
+    return bboxes;
+}
+
+function scrapper(params) {
+    console.log(getSearchBBoxes(params))
 }
 
 module.exports = {scrapper, valid};
