2e827b3d300f7ca03fd5ba79fd86a8931d452e26
Anonymous Developper <anonymous.developper@proton.me>
2024-11-24 16:03:25 +0100

    Create command to wipe all cameras

---
+++ project/commands/wipe.js
@@ -0,0 +1,29 @@
+const {question} = require("../libs/ui");
+const Camera = require("../models/Camera");
+
+function getArgs() {
+    return {
+        subject: async subject => {
+            if (subject === undefined || subject.toLowerCase() !== "cameras")
+                return {success: false, msg: "You have to mention what you want to wipe"}
+
+            return {success: true, data: subject.toLowerCase()};
+        }
+    }
+}
+
+function example() {
+    return  "\nnode console.js wipe cameras"
+}
+
+async function execute(_params) {
+    const res = await question("Are you SURE do you want wipe all cameras (Y/n) ?  ");
+    if (!["yes","y","oui","o"].some(str => str === res.toLowerCase())) {
+        console.log("no")
+        return;
+    }
+    await Camera.deleteMany({});
+    console.log("All cameras deleted successfully");
+}
+
+module.exports = {getArgs, example, execute}
